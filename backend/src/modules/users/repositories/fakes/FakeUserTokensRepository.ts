e6f5ae36250abd803a00ddfc8eddde54acdf14bc
Julio Felipe <julio.felipe@casaldev.com.br>
2020-07-31 06:30:50 -0300

    test: password reset

--- backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -5,7 +5,7 @@ import IUserTokensRepository from '@modules/users/repositories/IUserTokensReposi
 import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
 
 class FakeUserTokensRepository implements IUserTokensRepository {
-  private usertokens: UserToken[] = [];
+  private userTokens: UserToken[] = [];
 
   public async generate(user_id: string): Promise<UserToken> {
     const userToken = new UserToken();
@@ -16,7 +16,15 @@ class FakeUserTokensRepository implements IUserTokensRepository {
       user_id,
     });
 
-    this.usertokens.push(userToken);
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
 
     return userToken;
   }
