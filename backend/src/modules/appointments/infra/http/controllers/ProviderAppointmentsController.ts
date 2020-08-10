b69f3bd66b27369e6241d82eb49ffe49dfe6b7e4
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-10 05:46:20 -0300

    criação da agenda do prestador

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const listProviderAppointments = container.resolve(
+      ListProviderAppointmentsService,
+    );
+
+    const appointment = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(appointment);
+  }
+}
