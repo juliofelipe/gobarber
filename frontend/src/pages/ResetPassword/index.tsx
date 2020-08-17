bf49ff6aaf2f0fffa503817145fe4b826ea0ef93
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-17 06:50:10 -0300

    feat: implementando redefinição

--- frontend/src/pages/ResetPassword/index.tsx
+++ frontend/src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 
@@ -15,6 +15,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -28,6 +29,7 @@ const SignIn: React.FC = () => {
 
   // eslint-disable-next-line
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -46,7 +48,30 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        // history.push('/signin');
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        if (!token) {
+          addToast({
+            type: 'error',
+            title: 'Erro ao resetar senha',
+            description:
+              'Ocorreu um erro ao resetar sua senha, tente novamente.',
+          });
+          return;
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
+        history.push('/');
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
           const errors = getValidationErrors(error);
@@ -63,7 +88,7 @@ const SignIn: React.FC = () => {
         });
       }
     },
-    [addToast]
+    [addToast, history, location.search]
   );
 
   return (
