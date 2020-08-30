cd1d0f021b3ea7572b516f7a07f0734cfe73d996
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-30 07:11:20 -0300

    criando picker de data

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather'
-import api from '../../services/api';
+import { Platform } from 'react-native'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
+import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 
 import {
@@ -16,6 +18,10 @@ import {
   ProviderContainer,
   ProviderAvatar,
   ProviderName,
+  Calendar,
+  Title,
+  OpenDatePickerButton,
+  OpenDatePickerButtonText,
 } from './styles';
 
 
@@ -34,7 +40,11 @@ const CreateAppointment: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute()
   const { goBack } = useNavigation();
+
   const routeParams = route.params as RouteParams;
+
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   const [providers, setProviders] = useState<Provider[]>([]);
   const [selectedProvider, setSelectedProvider] = useState(routeParams.providerId);
@@ -53,35 +63,66 @@ const CreateAppointment: React.FC = () => {
     setSelectedProvider(providerId)
   }, []);
 
+  const handleToggleDatePicker = useCallback(() => {
+    setShowDatePicker((state) => !state);
+  }, []);
+
+  const handleDateChanged = useCallback((event: any, date: Date | undefined) => {
+    if (Platform.OS === 'android') {
+      setShowDatePicker(false);
+    }
+
+    setSelectedDate(date);
+  }, []);
+
   return (
-  <Container >
-    <Header>
-      <BackButton onPress={navigateBack}>
-        <Icon name="chevron-left" size={24} color="#999591"/>
-      </BackButton>
+    <Container >
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
 
-      <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
 
-      <UserAvatar source={{ uri: user.avatar_url }} />
-    </Header>
-    <ProvidersListContainer>
-      <ProvidersList
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={providers}
-        keyExtractor={provider => provider.id}
-        renderItem={({ item: provider }) => (
-          <ProviderContainer onPress={() => handleSelectProvider(provider.id)}
-            selected={provider.id === selectedProvider}
-          >
-            <ProviderAvatar source={{ uri: provider.avatar_url }} />
-            <ProviderName selected={provider.id === selectedProvider}>{provider.name}</ProviderName>
-          </ProviderContainer>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer onPress={() => handleSelectProvider(provider.id)}
+              selected={provider.id === selectedProvider}
+            >
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName selected={provider.id === selectedProvider}>{provider.name}</ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
+
+      <Calendar>
+        <Title>Escolha a data</Title>
+
+        <OpenDatePickerButton onPress={handleToggleDatePicker} >
+            <OpenDatePickerButtonText>Selecionar outra data</OpenDatePickerButtonText>
+          </OpenDatePickerButton>
+
+        {showDatePicker && (
+          <DateTimePicker
+            mode="date"
+            display="calendar"
+            onChange={handleDateChanged}
+            textColor="#f4ede8"
+            value={selectedDate}
+          />
         )}
-      />
-  </ProvidersListContainer>
-  </Container>
+      </Calendar>
+    </Container>
   );
+
 };
 
 export default CreateAppointment;
